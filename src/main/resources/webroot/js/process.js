a0b0fabe850b39ba0f1dfa8a4d07f8891206a9ea
Levy Moreira <levy.moreira@mastercard.com>
2019-07-03 11:33:56 +0100

    Update code example to match with website documentation

--- src/main/resources/webroot/js/process.js
+++ src/main/resources/webroot/js/process.js
@@ -1,61 +1,66 @@
 function createInput(name, value) {
-  var input = document.createElement("input");
-  input.setAttribute("type", "hidden");
+  var input = document.createElement('input');
+  input.setAttribute('type', 'hidden');
   input.setAttribute('name', name);
   input.setAttribute('value', value);
   return input;
 }
 
+function createSecure3dForm(data) {
+  var secure3dData = data['3dsecure'];
+  var secure3dForm = document.createElement('form');
+  secure3dForm.setAttribute('method', 'POST');
+  secure3dForm.setAttribute('action', secure3dData.acsUrl);
+  secure3dForm.setAttribute('target', 'secure3d-frame');
+
+  var merchantDetails = secure3dData.md;
+  var parReq = secure3dData.paReq;
+  var termUrl = secure3dData.termUrl;
+
+  secure3dForm.append(createInput('PaReq', parReq));
+  secure3dForm.append(createInput('TermUrl', termUrl));
+  secure3dForm.append(createInput('MD', merchantDetails));
+
+  return secure3dForm;
+}
+
 function processPayment() {
-
-  $.post("/pay", {
+  var payload = {
     cc_number: $('#cc-number').val(),
-    cc_exp_month : $('#cc-exp-month').val(),
-    cc_exp_year : $('#cc-exp-year').val(),
-    cc_cvc : $('#cc-cvc').val(),
-    currency : $('#currency').val(),
-    amount : $('#amount').val()
-  }, function(d, status, jq) {
-    console.log(d);
+    cc_exp_month: $('#cc-exp-month').val(),
+    cc_exp_year: $('#cc-exp-year').val(),
+    cc_cvc: $('#cc-cvc').val(),
+    currency: $('#currency').val(),
+    amount: $('#amount').val()
+  };
 
-    var token = d.token;
-    var currency = d.currency
-    if (d['3dsecure'].enrolled) {
+  $.post('/pay', payload, function (response) {
+    var token = response.token;
+    var currency = response.currency;
 
-      var content = document.createElement('div');
-
-      var secure3dForm = document.createElement('form');
-      secure3dForm.setAttribute("method", "POST");
-      secure3dForm.setAttribute("action", d['3dsecure'].acsUrl);
-      secure3dForm.setAttribute("target", "secure3d-frame");
-
-      var merchantDetails = d['3dsecure'].md;
-      var parReq = d['3dsecure'].paReq;
-      var termUrl = d['3dsecure'].termUrl;
-
-      secure3dForm.append(createInput('PaReq', parReq));
-      secure3dForm.append(createInput('TermUrl', termUrl));
-      secure3dForm.append(createInput('MD', merchantDetails));
-      content.appendChild(secure3dForm);
-
+    if (response['3dsecure'].enrolled) {
+      var secure3dForm = createSecure3dForm(response);
       var iframeNode = $('#secure3d-frame');
-      $(content).insertAfter(iframeNode);
+
+      $(secure3dForm).insertAfter(iframeNode);
       iframeNode.show();
 
-      var processPaymentToken = function(data) {
-        console.log("Processing token...");
-        window.removeEventListener('message', processPaymentToken);
-        var parsedJson = JSON.parse(data.data);
-        if (parsedJson.secure3d.authenticated) {
-          $.post("/complete", {
+      var process3dSecureCallback = function (threedsResponse) {
+        console.log('Processing 3D Secure callback...');
+        window.removeEventListener('message', process3dSecureCallback);
+
+        var simplifyDomain = 'https://simplify.com'; // For UAT you can use 'https://uat.simplify.com'
+
+        if (threedsResponse.origin === simplifyDomain && JSON.parse(threedsResponse.data)['secure3d']['authenticated']) {  // JSON.parse(data.data)['secure3d.authenticated']) {
+          var completePayload = {
             amount: 1500,
             currency: currency,
             description: 'description',
             token: token
-          }, function(dd, sstatus, jjqq) {
-            console.log(dd);
+          };
 
-            if (dd.success) {
+          $.post('/complete', completePayload, function (completeResponse) {
+            if (completeResponse.success) {
               $('#simplify-payment-form').hide();
               $('#simplify-success').show();
             }
@@ -64,19 +69,17 @@ function processPayment() {
         }
       };
 
-      $('#secure3d-frame').on('load', function() {
-        window.addEventListener('message', processPaymentToken);
+      iframeNode.on('load', function () {
+        window.addEventListener('message', process3dSecureCallback);
       });
 
       secure3dForm.submit();
     }
-
   });
-
 }
 
-$(document).ready(function() {
-  $("#simplify-payment-form").on("submit", function() {
+$(document).ready(function () {
+  $('#simplify-payment-form').on('submit', function () {
     processPayment();
     return false;
   });
